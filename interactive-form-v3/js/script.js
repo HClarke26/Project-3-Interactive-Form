e57de291984991bc5478881c09b7a6ebb920194c
HClarke26 <harryclarke26@gmail.com>
2022-03-28 17:24:48 +0100

    Update script.js

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -1,16 +1,17 @@
 // Project 3 - Interactive Form
 
-//Name Field
+/********************Name Field********************/
 const nameInput = document.getElementById("name");
+//Setting the focus to the name field whe the page is loaded
 nameInput.focus();
 
 
-//Job Role section
+/********************Job Role Section********************/
 const jobRoleInput = document.getElementById("title");
 const otherJobRoleInput = document.getElementById("other-job-role");
-
+//Input for other job is hidden
 otherJobRoleInput.style.visibility = 'hidden';
-
+//If other is selected from the drop down menu, other job role section becomes visible
 jobRoleInput.addEventListener('click', (e) => {
     if (e.target.value === 'other') {
         otherJobRoleInput.style.visibility = 'visible';
@@ -20,17 +21,18 @@ jobRoleInput.addEventListener('click', (e) => {
 });
 
 
-//T shirt section
+/********************T-shirt Section********************/
 const designInput = document.getElementById("design");
 const colorInput = document.getElementById("color");
-
+//Colour option is disabled 
 colorInput.disabled = true ;
 
 designInput.addEventListener('change', (e) => {
     colorInput.disabled = false ;
     for (let i = 0 ; i < colorInput.length; i++) {
-        // conditional (Ternary Operator)
+        //Conditional (Ternary Operator)
         colorInput.value = colorInput[i].value ? 'Select a design theme above': colorInput[i].value;
+        //Conditional - If a design is chosen, the colour option is enabled with relevant colours
         if (e.target.value === colorInput[i].getAttribute('data-theme')){             
             colorInput[i].hidden = false;           
         } else {
@@ -40,11 +42,10 @@ designInput.addEventListener('change', (e) => {
 });
 
 
-//Register for activities
+/********************Activities Section********************/
 const activities = document.getElementById('activities');
 const finalCost = document.getElementById('activities-cost');
 let totalCost = 0;
-
 //Event listener that adds up total charges of activities
 activities.addEventListener('change', (e) => {
     const activityCost =+ e.target.getAttribute('data-cost');
@@ -57,7 +58,7 @@ activities.addEventListener('change', (e) => {
 })
 
 
-//Payment information section
+/********************Payment Section********************/
 const paymentOption = document.getElementById('payment');
 paymentOption.children[1].setAttribute('selected', true);
 const creditCard = document.getElementById('credit-card');
@@ -67,9 +68,10 @@ bitcoin.style.display = 'none';
 const paypal = document.getElementById('paypal');
 paypal.style.display = 'none';
 
+//Event listenter for a change in the payement option. 
 paymentOption.addEventListener('change', (e) => {
     const selectedPayment = e.target.value;
-
+//Conditional to display correct styles depending on payment option selected
     if (selectedPayment === 'bitcoin') {
         bitcoin.style.display = 'block';
         paypal.style.display = 'none';
@@ -81,10 +83,11 @@ paymentOption.addEventListener('change', (e) => {
     } else {
         paypal.style.display = 'none';
         bitcoin.style.display = 'none'; 
+        creditCard.style.display = 'block';
     }
 })
 
-//Form validation section
+/********************Form Validation Section********************/
 const emailInput = document.getElementById('email');
 const cardNumberInput = document.getElementById('cc-num');
 const zipCodeInput = document.getElementById('zip');
@@ -92,8 +95,11 @@ const cvvInput = document.getElementById('cvv');
 const form = document.querySelector('form');
 
 //Helper functions
+
 function isValidName () {
+    //Validating name field cannot be blank
     const nameRegEx = /^[\w]+\s*[\w]+$/.test(nameInput.value);
+    //Conditional to apply relevant styles depedning on user input 
     if (nameRegEx === true) {
         nameInput.parentNode.className='valid';
         nameInput.parentNode.lastElementChild.style.display = 'none';
@@ -101,13 +107,15 @@ function isValidName () {
     } else {
         nameInput.parentNode.className='not-valid';
         nameInput.parentNode.lastElementChild.style.display = 'block';
-        nameInput.parentNode.lastElementChild.textContent = 'Invalid input. This field must only contain letters.'
+        nameInput.parentNode.lastElementChild.textContent = 'Invalid input. This field cannot be blank and must only contain letters.'
         return nameRegEx
     }
 }
 
 function isValidEmail () {
+    //Validating email is in the format 'input' + '@' + 'input' + '.' + 'input'
     const emailRegEx =  /^[^@]+@[^@.]+\.[a-z]+$/i.test(emailInput.value);
+    //Conditional to apply relevant styles depedning on user input 
     if (emailRegEx === true) {
         emailInput.parentNode.className='valid';
         emailInput.parentNode.lastElementChild.style.display = 'none';
@@ -122,19 +130,22 @@ function isValidEmail () {
 
 function isValidActivities () {
     let checkedBox = document.querySelectorAll('[type="checkbox"]:checked')
+    let activitiesBox = document.getElementById('activities-box')
     if (checkedBox.length > 0) {
-        activities.parentNode.className='valid';
-        activities.parentNode.lastElementChild.style.display = 'none';
+        activitiesBox.parentNode.className='valid';
+        activitiesBox.parentNode.lastElementChild.style.display = 'none';
         return true
     } else {
-        activities.parentNode.className='not-valid';
-        activities.parentNode.lastElementChild.style.display = 'block'
-        activities.lastElementChild.textContent = 'Please select at least one activity.'
+        activitiesBox.parentNode.className='not-valid';
+        activitiesBox.parentNode.lastElementChild.style.display = 'block'
+        activitiesBox.lastElementChild.textContent = 'Please select at least one activity.'
     }   
 } 
 
 function isValidCardNumber () {
+    //Validating card number is all numbers and between 13 and 16 digits
     const cardNumberRegEx = /^\d{13,16}$/.test(cardNumberInput.value)
+    //Conditional to apply relevant styles depedning on user input 
     if (cardNumberRegEx === true) {
         cardNumberInput.parentNode.className='valid';
         cardNumberInput.parentNode.lastElementChild.style.display = 'none';
@@ -147,7 +158,9 @@ function isValidCardNumber () {
     }
 }
 function isValidZipCode () {
+    //Validating zip code is five digits from 0-9
     const zipRegEx = /^[0-9]{5}$/.test(zipCodeInput.value)
+    //Conditional to apply relevant styles depedning on user input 
     if (zipRegEx === true) {
         zipCodeInput.parentNode.className='valid';
         zipCodeInput.parentNode.lastElementChild.style.display = 'none';
@@ -161,7 +174,9 @@ function isValidZipCode () {
 }
 
 function isValidCvv () {
+    //Validating zip code is three digits from 0-9
     const CvvRegEx = /^\d{3}$/.test(cvvInput.value)
+    //Conditional to apply relevant styles depedning on user input 
     if (CvvRegEx === true) {
         cvvInput.parentNode.className='valid';
         cvvInput.parentNode.lastElementChild.style.display = 'none';
@@ -174,23 +189,23 @@ function isValidCvv () {
     }
 }
 
+//NEED HELP WITH THIS FUNCTION
 function isValidPayment () {
     if (paymentOption.value === 'credit-card') {
         isValidCardNumber()
+        isValidZipCode() 
         isValidCvv()
-        isValidZipCode()  
-        }  
-    return true     
+        }    
     }
 
     
 //Event listener for submitting the form   
 form.addEventListener('submit', (e) => {
     
-    if (isValidName() && isValidEmail() && isValidActivities() && isValidPayment() && isValidCvv() && isValidZipCode()) {
+    if (isValidName() && isValidEmail() && isValidActivities() && isValidPayment()) {
         console.log('form submited');
-    } else {
-        e.preventDefault();
+    } else { 
+        e.preventDefault(); //If there are invalid inputs the form will be prevented from submitting
         isValidName();
         isValidEmail(); 
         isValidPayment();
@@ -199,9 +214,9 @@ form.addEventListener('submit', (e) => {
     }
 });
 
-//Accessibility section
+/********************Accessibility Section********************/
 const checkboxInput = document.querySelectorAll('#activities-box input');
-console.log(checkboxInput)
+//Condtional to apply focus to relevant checkbox
 for(let i = 0; i < checkboxInput.length; i++) {
     checkboxInput[i].addEventListener('focus', (e) => {
         checkboxInput[i].parentElement.classList.add('focus');
